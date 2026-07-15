38a974a25140a625126c34f1752c4891ebff1f93
Initium <sales.initium2026@gmail.com>
2026-07-15 22:17:59 +0800

    Patient controller and CRUD endpoints added

---
+++ ClinicAPI/Controllers/PatientController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicAPI.Data;
+using ClinicAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/patient")]
+    public class PatientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Patient>>> GetPatients()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Patient>> GetPatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return patient;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Patient>> CreatePatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetPatient),
+                new { id = patient.Id },
+                patient);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePatient(int id, Patient patient)
+        {
+            if (id != patient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patients.Remove(patient);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
