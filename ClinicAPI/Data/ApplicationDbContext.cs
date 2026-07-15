57f6441c7a7f1d298e69282b3b298138d76fe39d
Initium <sales.initium2026@gmail.com>
2026-07-15 21:24:34 +0800

    Add project files.

---
+++ ClinicAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ClinicAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients => Set<Patient>();
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ ClinicAPI/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿namespace ClinicAPI.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Gender { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string Address { get; set; }
+    }
+}
